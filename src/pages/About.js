5e6ea7e32d373aa13b85314d157a9a81fc385b01
sarathgoli <golisarathkumar141@gmail.com>
2021-07-16 11:35:25 +0530

    about changed

--- src/pages/About.js
+++ src/pages/About.js
@@ -7,9 +7,31 @@ import { selectUser } from "../components/redux_code/userSlice";
 import { useSelector } from "react-redux";
 import { login_redux } from "../components/redux_code/userSlice";
 
+import { makeStyles } from "@material-ui/core/styles";
+import { withStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+const useStyles = makeStyles({
+  root: {
+    minWidth: 275
+  },
+  bullet: {
+    display: "inline-block",
+    margin: "0 2px",
+    transform: "scale(0.8)"
+  },
+  title: {
+    fontSize: 14
+  },
+  pos: {
+    marginBottom: 12
+  }
+});
 const About = () => {
     const history = useHistory();
-
+    const classes = useStyles();
     // const user = useSelector(selectUser);
   
     const dispatch = useDispatch();
@@ -39,7 +61,17 @@ const About = () => {
         });
     }, []);
     return (
-        <Hero/>
+        <>
+          <div className="displayingpost" style={{width:'80%',margin:'3rem auto',font:"bold"}}>
+          <Card className={classes.root} variant="outlined">
+          <CardContent>
+          This website enables users to write blogs about their interview experiences.Besides writing blogs,users can read and search for the content they want by using filters provided in the page.
+            <br/>
+            <span style={{color:"#f03211"}}>To have a experience this website.Please login first</span>
+          </CardContent>
+          </Card>
+          </div>
+        </>
     )
 }
 export default About;
